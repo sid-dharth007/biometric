f64567eeb6cfc99c4630b9c9ad942764b27b84b5
sid-dharth007 <sidsingh006vf@gmail.com>
2026-02-20 11:26:55 +0530

    Delete biometric attendance directory

--- biometric attendance/models/student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const StudentSchema = new mongoose.Schema({
-  name: String,
-  roll: String,
-  faceDescriptor: Array
-});
-
-module.exports = mongoose.model("Student", StudentSchema);
